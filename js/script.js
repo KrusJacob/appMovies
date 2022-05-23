10c5cfd88962063094d4e81711a45da0b109c9e3
Jacob <coneter@mail.com>
2022-05-23 20:23:34 +0300

    add fucntion: start

---
+++ js/script.js
@@ -0,0 +1,44 @@
+// Домашнее задание:
+let numberOfFilms;
+
+function start() {
+  numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+  while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+  }
+}
+
+start();
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: {},
+  privat: false,
+};
+
+for (let i = 0; i < 2; i++) {
+  const a = prompt("Один из просмотренных последних фильмов?", ""),
+    b = prompt("На сколько оцените его?", "");
+
+  if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log("done");
+  } else {
+    console.log("error");
+    i--;
+  }
+}
+
+if (personalMovieDB.count < 10) {
+  console.log("Просмотренно довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+  console.log("Вы киноман");
+} else {
+  console.log("Произошла ошибка");
+}
+
+console.log(personalMovieDB);
